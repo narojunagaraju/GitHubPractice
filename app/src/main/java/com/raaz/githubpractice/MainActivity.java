076781310f70c6d60d120f21b11810bce8687bad
raaz_naroju <narojunagarajun@gmail.com>
2018-06-07 16:09:34 +0530

    Initial commit

---
+++ app/src/main/java/com/raaz/githubpractice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.raaz.githubpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //initial commit
+}
